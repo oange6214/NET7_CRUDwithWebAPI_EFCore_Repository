f522a47ad29c7d9dca08a918a344239c085e8212
Pohan <oange6214@gmail.com>
2023-10-28 20:05:54 +0800

    Add project files.

---
+++ FormulaApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
